3bb48360b7f02b934f8933f5cfb0ac35d275256e
srrahi <60713419+srrahi@users.noreply.github.com>
2022-04-13 20:55:04 +0200

    Update next.config.js

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  
+    "redirects": [
+        {
+            "source": "/(.*)",
+            "destination": "https://timezones-api.datasette.io/$1",
+            "statusCode": 301
+        }
+    ]
+
 }
 
 module.exports = nextConfig
